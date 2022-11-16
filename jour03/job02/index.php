7e67c152a155824bed61b7d2c8f442cc532d6280
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-16 11:45:24 +0100

    Uttilisation du isset a la place du strlen

--- jour03/job02/index.php
+++ jour03/job02/index.php
@@ -17,9 +17,19 @@ Ex. : Tu e ntnssrn edsdn etmscmelslre osl li.
  <body>
  <?php
 $str = 'Tous ces instants seront perdus dans le temps comme les larmes sous la pluie.';
-$long = strlen($str);
+
+// $long = strlen($str);            // Pour verifier les resultats de la boucle while.
+// echo $long. '<br>';
+
+$long = 0;
+$i=0;
+while (isset($str[$i]) == true) {   // Pour calculé la langueure de la chaine du caractere.
+   $long = $long + 1;
+   $i = $i +1;
+}
+
 for ($i =0; $i < $long; $i++) {
-   if ($i % 2 == 0) {
+   if ($i % 2 == 0) {               // Pour afficher que les caractere impaire.  
       echo $str[$i];
    }
 }
--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -1,6 +1,10 @@
 <!-- 
     ***     Mohamed HABBAAINA, le 16 nov 2022       ***
 
+Créez une variable de type string nommée $str et affectez y le texte :
+“I'm sorry Dave I'm afraid I can't do that”.
+Créez un algorithme qui parcourt cette chaîne et affiche uniquement les voyelles.
+Ex. : IoyaeIaaiIaoa
  -->
   <!DOCTYPE html>
  <html lang="fr">
@@ -12,7 +16,22 @@
  </head>
  <body>
  <?php
+$str = 'I\'m sorry Dave I\'m afraid I can\'t do that';
 
+// $long = strlen($str);
+// echo $long. '<br>';
+
+$long = 0;
+$i=0;
+while (isset($str[$i]) == true) {   // Pour calculé la langueure de la chaine du caractere.
+   $long = $long + 1;
+   $i = $i +1;
+}  echo $long;
+for ($i =0; $i < $long; $i++) {
+   if ($str[$long] == 'a') {
+      echo $str[$long];
+   }
+}
 ?>
 </body>
 </html>
