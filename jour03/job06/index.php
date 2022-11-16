97e082f6c82c848e182f8498a4f9074cebb58400
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-16 19:09:16 +0100

    algorithme qui écrit une chaine à l’envers

--- jour03/job06/index.php
+++ jour03/job06/index.php
@@ -1,6 +1,10 @@
 <!-- 
     ***     Mohamed HABBAAINA, le 16 nov 2022       ***
 
+Créez une variable de type string nommée $str et affectez y le texte :
+“Les choses que l'on possède finissent par nous posséder."
+Créez un algorithme qui parcourt et écrit cette chaine à l’envers.
+Ex. : redessop suon rap tnessinif edessop no'l euq sesohc seL
  -->
   <!DOCTYPE html>
  <html lang="fr">
@@ -12,7 +16,22 @@
  </head>
  <body>
  <?php
-
-?>
+   $str= 'Les choses que l\'on possède finissent par nous posséder.';
+   $long = 0;
+   $i=0;
+   while (isset($str[$i]) == true) {   // Pour calculé la langueure de la chaine du caractere.
+      $long = $long + 1;
+      $i = $i +1;
+   }
+   $str_reverse = [];                  // Le reverse de la chaine $str.
+   // echo $long;
+   for ($i = 0; $i < $long; $i++) {
+       $str_reverse[$i] = $str[($long - $i -1)];
+    }  //echo $str;
+    //print_r($str_reverse);
+    foreach($str_reverse as $str_rev) {
+      echo $str_rev;                   // L'affichage du reverse.
+    }
+   ?>
 </body>
 </html>
--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -1,6 +1,11 @@
 <!-- 
     ***     Mohamed HABBAAINA, le 16 nov 2022       ***
 
+Créez une variable de type string nommée $str et affectez y le texte :
+“Certaines choses changent, et d'autres ne changeront jamais.”
+Créer un algorithme qui parcourt cette string en remplaçant le premier caractère par le
+deuxième, le deuxième par le troisième etc.. et le dernier par le premier.
+Ex. : Ertaines choses changent, et d'autres ne changeront jamais.c
  -->
   <!DOCTYPE html>
  <html lang="fr">
