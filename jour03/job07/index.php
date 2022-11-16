62221558ca2f6fce57b0a695a32203788f54cafd
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-16 19:22:41 +0100

    algorithme qui remplaçant le premier caractère par le deuxième, le deuxième par le troisième etc.. dans une chaine de caractere

--- jour03/job07/index.php
+++ jour03/job07/index.php
@@ -17,7 +17,24 @@ Ex. : Ertaines choses changent, et d'autres ne changeront jamais.c
  </head>
  <body>
  <?php
+$str = 'Certaines choses changent, et d\'autres ne changeront jamais.';
+$long = 0;
+$i=0;
+while (isset($str[$i]) == true) {   // Pour calculé la langueure de la chaine du caractere.
+   $long = $long + 1;
+   $i = $i +1;
+} echo "$str <br>";
+$str_2 = [];                  // La nouvelle chaine $str.
 
+// echo $long;
+for ($i = 0; $i < $long; $i++) {
+    $str_2[$i] = $str[($i +1)];
+ }  //echo $str;
+ $str_2[$long] = $str[0];
+ //print_r($str_reverse);
+ foreach($str_2 as $str_02) {
+   echo $str_02;                   // L'affichage de la nouvelle chaine.
+ }
 ?>
 </body>
 </html>
