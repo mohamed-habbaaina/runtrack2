803908db48cd72bdda073fa85dc3d8d4563c7903
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-23 22:40:11 +0100

    job 3

--- jour08/job3/index.php
+++ jour08/job3/index.php
@@ -7,5 +7,27 @@ session. Afficher l’ensemble des prénoms.
 Ajoutez un bouton nommé “reset” qui permet de réinitialiser la liste.
  -->
 <?php
-
+session_start();
+$_SESSION['prenom'] = $_GET['prenom'];
+echo 'Le prénom de l\'utilisateur est: ' . $_SESSION['prenom'];
+if (isset($_GET['reset'])){
+    $_SESSION['prenom'] = null;
+}
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les sessions</title>
+</head>
+<body>
+<form action="" method="get">
+    <label for="prenom" >prénom</label>
+    <input type="text" name="prenom" id="prenom">
+
+    <button type="submit" name="reset">reset</button>
+</form>    
+</body>
+</html>
