3fd31bce342323577220f764e8f01b217b991b99
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-24 15:13:53 +0100

    requete sql 'étudiants qui ont plus de 18 ans'

---
+++ jour09/job7/job.sql
@@ -0,0 +1,5 @@
+-- Mohamed HABBAAINA le : jeu. 24 nov. 2022
+
+-- Ecrivez dans le fichier “job07.sql” une requête permettant de sélectionner l’ensemble
+-- des informations des étudiants qui ont plus de 18 ans.
+SELECT * FROM `etudiants` WHERE naissance < '2004-11-24';
