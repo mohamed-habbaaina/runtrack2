ba0171d63bf0a36ec3f253ca387a9e96c1c82892
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-24 13:55:18 +0100

    requête permettant de sélectionner le nom et la capacite de la table “salles”

--- jour09/job3/job3.sql
+++ jour09/job3/job3.sql
@@ -1,4 +1,9 @@
 -- Mohamed HABBAAINA
 -- Généré le : jeu. 24 nov. 2022 à 13:06
 
-SELECT * FROM etudiants;
+-- Maintenant que vous avez créé une base de données, des tables et que vous y avez
+-- ajouté des données, vous allez pouvoir les manipuler.
+-- Ecrivez dans le fichier “job03.sql” une requête permettant de sélectionner l’ensemble
+-- des champs de la table etudiants.
+
+SELECT * FROM `etudiants`;
---
+++ jour09/job4/job4.sql
@@ -0,0 +1,7 @@
+-- Mohamed HABBAAINA
+-- Généré le : jeu. 24 nov. 2022 à 13:06
+
+-- Ecrivez dans le fichier “job04.sql” une requête permettant de sélectionner le nom et la
+-- capacite de la table “salles”.
+
+SELECT `nom` FROM `salles`;
