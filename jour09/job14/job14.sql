b507edc649ce4472f18d8d6c5465a181003a7f1f
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-24 16:02:41 +0100

    sql BETWEEN

---
+++ jour09/job14/job14.sql
@@ -0,0 +1,5 @@
+-- Mohamed HABBAAINA le : jeu. 24 nov. 2022
+
+-- Ecrivez dans le fichier “job14.sql” une requête permettant de sélectionner le prenom, le
+-- nom et la date de naissance des étudiants qui sont nés entre 1998 et 2018.
+SELECT * FROM `etudiants` WHERE naissance BETWEEN '1998-01-01' AND '2018-12-31';
