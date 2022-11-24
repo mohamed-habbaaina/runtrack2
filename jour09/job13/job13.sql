e54c20337c6bde13b6d516d694e0f0f96555d050
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-24 15:56:03 +0100

    la capacité moyenne des salles AVG(capacite)

---
+++ jour09/job13/job13.sql
@@ -0,0 +1,5 @@
+-- Mohamed HABBAAINA le : jeu. 24 nov. 2022
+
+-- Ecrivez dans le fichier “job13.sql” une requête permettant de sélectionner la capacité
+-- moyenne des salles.
+SELECT AVG(capacite) FROM `salles`;
