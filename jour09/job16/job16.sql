9c5a24e6872f6c371c039d422300bc7e67566ce0
mohamed-habbaaina <mohamed.habbaaina@laplateforme.io>
2022-11-28 09:00:21 +0100

    preparation job 16

---
+++ jour09/job16/job16.sql
@@ -0,0 +1,6 @@
+-- Mohamed HABBAAINA le : jeu. 24 nov. 2022
+
+-- Ecrivez dans le fichier “job16.sql” une requête permettant de récupérer le nom de
+-- l'étage ayant la salle avec la plus grande capacité (et afficher aussi le nom de cette salle
+-- ainsi que sa capacité).
+-- Dans ce résultat, la colonne “nom” de la salle doit être renommée en “Biggest Room”
